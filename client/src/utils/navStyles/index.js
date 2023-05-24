dd081162f784762145ee5b292106c6d7a4b7ff1b
Michele Zucca <michele.zucca81@gmail.com>
2023-05-24 15:28:44 +0300

    Adjusted nav styles

--- client/src/utils/navStyles/index.js
+++ client/src/utils/navStyles/index.js
@@ -1,10 +1,9 @@
 export const activeStyle = {
-    borderBottom: "3px solid var(--white)",
-    color: "var(--white)",
+    borderBottom: "3px solid var(--primary)",
     fontWeight: "700",
     textDecoration: "none"
   }
 
 export const activeStyleScrolled = {
-    borderBottom: "3px solid rgb(75, 74, 74)"
+    borderBottom: "3px solid #fff"
 }
